fe47eca3acd1f9fa1d5d7819f088d1c143e607e5
derekfq <quaresmaderek@gmail.com>
2022-03-27 16:07:36 -0300

    reactApp creation

---
+++ pathfront/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+import PathFinding from './PathFindingFront/PathFindingVisualizer';
+function App() {
+  return (
+    <div className="App">
+      <PathFinding></PathFinding>
+    </div>
+  );
+}
+
+export default App;
